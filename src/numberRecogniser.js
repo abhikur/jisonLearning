6f24881a1758a1e2c7e5ec4f1b4d0a5606a7434b
Abhishek Thakur <thakura204@gmail.com>
2016-11-11 20:52:53 +0530

    -Decimal and hexaDecimal recogniser are added

--- src/numberRecogniser.js
+++ src/numberRecogniser.js
@@ -1,21 +1,43 @@
 var Parser = require('jison').Parser;
 var numberRecogniser = {};
 
-var grammer = {
-	"lex":{
-		"rules": [
-			["\\s+", "/* skip whitespace */"],
-			["[0-9]+", "return 'Num'"]
-		]
-	},
-	"bnf": {
-		"A": ["A Num", "Num"]
+function generateGrammer() {
+	var grammer = {};
+	grammer.lex = {rules:[["\\s+", "/* skip whitespace */"]]};
+	grammer.bnf = {};
+	return grammer;
+}
+
+function parse(grammer, toParse) {
+	var parser = new Parser(grammer);
+	var result = true;
+	try {
+		result = parser.parse(toParse);
+	}catch(e) {
+		return false;
 	}
+	return result;
 }
 
 numberRecogniser.recogniseInteger = function(integer) {
-	var parser = new Parser(grammer);
-	return parser.parse(integer);
+	var grammer = generateGrammer();
+	grammer.lex.rules.push(["[0-9]+", "return 'Num'"]);
+	grammer.bnf.A = ["A Num", "Num"];
+	return parse(grammer, integer);
+}
+
+numberRecogniser.recogniseDecimal = function(decimal) {
+	var grammer = generateGrammer();
+	grammer.lex.rules.push(["[0-9]+.[0-9]+", "return 'Num'"]);
+	grammer.bnf.A = ["A Num", "Num"];
+	return parse(grammer, decimal);
+}
+
+numberRecogniser.recogniseHexaDecimal = function(hexaDecimal) {
+	var grammer = generateGrammer();
+	grammer.lex.rules.push(["[a-f0-9]+", "return 'Num'"])
+	grammer.bnf.A = ["A Num", "Num"];
+	return parse(grammer, hexaDecimal);
 }
 
 module.exports = numberRecogniser;
--- test/numberRecogniserTest.js
+++ test/numberRecogniserTest.js
@@ -2,9 +2,22 @@ var recogniser = require('../src/numberRecogniser');
 var assert = require('assert');
 
 describe('numberRecogniser', function() {
-	describe('recogniseIntegers', function() {
+	describe('recogniser', function() {
 		it('should recognise simple itegers', function() {
 			assert.equal(true, recogniser.recogniseInteger("123"));
+			assert.equal(false, recogniser.recogniseInteger("123.0"));
+		}),
+
+		it('should recognise decimal numbers', function() {
+			assert.equal(true, recogniser.recogniseDecimal("123.0"));
+			assert.equal(true, recogniser.recogniseDecimal("123.101"));
+			assert.equal(true, recogniser.recogniseDecimal("123"));
+		}),
+
+		it('should recognise hexa-decimal numbers', function() {
+			assert.equal(true, recogniser.recogniseHexaDecimal("adfe3"));
+			assert.equal(true, recogniser.recogniseHexaDecimal("abc12c"));
+			assert.equal(false, recogniser.recogniseHexaDecimal("dfe34bc zxg"));
 		})
 	})
 })
